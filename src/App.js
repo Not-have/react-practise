bc3c592befad435b29fcfe12f1dacc8139e2ed40
Not-have <57132948+Not-have@users.noreply.github.com>
2022-04-08 16:42:29 +0800

    添加了claas

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import './App.css'
-import { Routes, Route, Link } from 'react-router-dom'
-import Home from './pages/home/Index.jsx'
-import Mine from './pages/mine/Mine.jsx'
-function App () {
-	return (
-		<div className="App">
-			<div>
-				<Link to="/home">about</Link>
-				<Link to="/mine">about</Link>
-			</div>
-			<Routes>
-				<Route path="/home" element={<Home />} />
-				<Route path="/mine" element={<Mine />} />
-			</Routes>
-		</div>
-	)
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/home/Index.jsx";
+import Mine from "./pages/mine/Mine.jsx";
+function App() {
+  return (
+    <div className="App">
+      <div className="menu">
+        <Link to="/home">about</Link>
+        <Link to="/mine">mine</Link>
+      </div>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/mine" element={<Mine />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
