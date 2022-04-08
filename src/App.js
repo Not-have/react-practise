8ac2e642a06e80deb0f8d6c97dee2d978cab29f4
Not-have <57132948+Not-have@users.noreply.github.com>
2022-04-08 17:28:24 +0800

    demo

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/home/Index.jsx";
 import Mine from "./pages/mine/Mine.jsx";
+import State from "./pages/setState是异步的/Index.jsx";
+import StateModify from "./pages/state中数组的修改/Index.jsx";
 function App() {
   return (
     <div className="App">
       <div className="menu">
         <Link to="/home">about</Link>
         <Link to="/mine">mine</Link>
+        <Link to="/state">state</Link>
+        <Link to="/stateModify">stateModify</Link>
       </div>
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/mine" element={<Mine />} />
+        <Route path="/state" element={<State />} />
+        <Route path="/stateModify" element={<StateModify />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   );
---
+++ src/pages/setState是异步的/Index.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+export default class App5 extends Component {
+  state = {
+    msg: "你好世界"
+  };
+  render() {
+    return (
+      <div>
+        <h2>{this.state.msg}</h2>
+        <button onClick={this.handleClick.bind(this)}>改变msg</button>
+      </div>
+    );
+  }
+  handleClick() {
+    // this.setState是异步还是同步呢？
+    this.setState({
+      msg: "hello world" // 修改msg
+    });
+    console.log(this.state.msg); // 同步则输出“hello world”，异步则输出“你好世界”
+
+    // 验证结果：this.setState是异步的
+  }
+}
